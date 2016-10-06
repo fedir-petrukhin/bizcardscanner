e3a4ac4211564aa80dafb37a01d578b574df895f
Fedir Petrukhin <fpetrukhin@gmail.com>
2016-10-06 17:35:58 +0300

    fixed

--- src/com/cci/bizcard/plugin/BizCardScanPlugin.java
+++ src/com/cci/bizcard/plugin/BizCardScanPlugin.java
@@ -33,7 +33,7 @@ public class BizCardScanPlugin extends CordovaPlugin {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if (resultCode == 0) {
+        if (resultCode == -1) {
             String vcfData = data.getStringExtra(OpenApi.EXTRA_KEY_VCF);
             callbackContext.success(vcfData);
         } else {
