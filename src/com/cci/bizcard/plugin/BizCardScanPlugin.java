a241c17d103ab821f0190ee8b81e972937fb4f20
Fedir Petrukhin <fpetrukhin@gmail.com>
2016-10-06 17:29:09 +0300

    fixed

--- src/com/cci/bizcard/plugin/BizCardScanPlugin.java
+++ src/com/cci/bizcard/plugin/BizCardScanPlugin.java
@@ -58,7 +58,7 @@ public class BizCardScanPlugin extends CordovaPlugin {
             };
 
             if ("scanPhoto".equals(action)) {
-                //this.cordova.setActivityResultCallback(this);
+                this.cordova.setActivityResultCallback(this);
                 openApi.recognizeCardByCapture(this.cordova.getActivity(), CARD_SCAN_CODE, params);
                 PluginResult r = new PluginResult(PluginResult.Status.NO_RESULT);
                 r.setKeepCallback(true);
