52daa2011f54591aa377a77a3675cd4172c067c5
Fedir Petrukhin <fpetrukhin@gmail.com>
2016-10-06 17:13:14 +0300

    fixed

--- src/com/cci/bizcard/plugin/BizCardScanPlugin.java
+++ src/com/cci/bizcard/plugin/BizCardScanPlugin.java
@@ -7,10 +7,12 @@ import android.content.Intent;
 import com.intsig.openapilib.OpenApi;
 import com.intsig.openapilib.OpenApiParams;
 
+import org.apache.cordova.*;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 
